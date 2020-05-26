81ccd2463c1005af22fb9e5eaba94c798b3b73fc
Dima Alekseev <alekseevda24@gmail.com>
2020-05-26 10:16:49 +0400

    Create ServiceContainer.java

---
+++ Twinkle/Swing/src/main/java/com/github/donkeyrit/javaapp/container/ServiceContainer.java
@@ -0,0 +1,28 @@
+package com.github.donkeyrit.javaapp.container;
+
+import com.github.donkeyrit.javaapp.database.DatabaseProvider;
+
+public class ServiceContainer {
+
+    private static ServiceContainer instance;
+
+    public static ServiceContainer getInstance() {
+        if (instance == null) {
+            instance = new ServiceContainer();
+        }
+        return instance;
+    }
+
+
+
+    private ServiceContainer() { }
+
+    private DatabaseProvider databaseProvider;
+    public DatabaseProvider getDatabaseProvider() {
+        return databaseProvider;
+    }
+    public void setDatabaseProvider(DatabaseProvider databaseProvider) {
+        this.databaseProvider = databaseProvider;
+    }
+
+}
