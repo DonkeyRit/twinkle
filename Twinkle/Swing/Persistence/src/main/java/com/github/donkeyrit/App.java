bfbdea206b67b6a001932bec7f9652ef0c876854
Dima Alekseev <alekseevda24@gmail.com>
2021-02-23 16:43:43 +0400

    [Twinkle-0003] Configure hibernate with PostgreSQL

--- Twinkle/Swing/Persistence/src/main/java/com/github/donkeyrit/App.java
+++ Twinkle/Swing/Persistence/src/main/java/com/github/donkeyrit/App.java
@@ -1,5 +1,16 @@
 package com.github.donkeyrit;
 
+import com.github.donkeyrit.model.Message;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  * Hello world!
  *
@@ -8,6 +19,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure() // configures settings from hibernate.cfg.xml
+                .build();
+        try {
+            SessionFactory sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+            Session session = sessionFactory.openSession();
+            session.persist(new Message("Hello World"));
+            session.close();
+            sessionFactory.close();
+        }
+        catch (Exception e) {
+            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+            // so destroy it manually.
+            StandardServiceRegistryBuilder.destroy( registry );
+        }
     }
 }
