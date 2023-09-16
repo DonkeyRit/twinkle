f3a745d5cc90411c67f128f680273fadcf5e90bf
Dima Alekseev <alekseevda24@gmail.com>
2023-09-16 18:27:44 +0200

    [Twinkle-#51][Swing] Update layout on PasswordUpdatePanel

--- twinkle-app/twinkle.main/src/main/java/com/github/donkeyrit/twinkle/panels/settings/PasswordUpdatePanel.java
+++ twinkle-app/twinkle.main/src/main/java/com/github/donkeyrit/twinkle/panels/settings/PasswordUpdatePanel.java
@@ -2,43 +2,56 @@ package com.github.donkeyrit.twinkle.panels.settings;
 
 import com.github.donkeyrit.twinkle.bll.services.interfaces.UserInfoService;
 import com.github.donkeyrit.twinkle.controls.input.JCustomPasswordField;
-import com.github.donkeyrit.twinkle.utils.AssetsRetriever;
 
 import javax.swing.border.TitledBorder;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Box;
 
 import com.google.inject.Inject;
 import java.util.Optional;
+import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Color;
-import java.awt.Image;
 
 public class PasswordUpdatePanel extends JPanel {
+
+	private static final Color DARK_BG_COLOR = new Color(50, 50, 50);
+    private static final Color DARK_FG_COLOR = Color.WHITE;
 
 	private final String OLD_PASSWORD_LABEL = "Enter old pasword";
 	private final String NEW_PASSWORD_LABEL = "Enter new password";
 	private final String REPEAT_NEW_PASSWORD_LABEL = "Repeat new password";
 	private final UserInfoService userInfoService;
 
+	private JLabel errorMessageLabel;
+
 	@Inject
 	public PasswordUpdatePanel(UserInfoService userInfoService) {
-		setLayout(null);
+		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        this.setBackground(DARK_BG_COLOR);
 		this.userInfoService = userInfoService;
 		initializeComponents();
 	}
 
 	private void initializeComponents() {
 		Box mainBox = Box.createVerticalBox();
-		mainBox.setBorder(new TitledBorder("Change Password"));
-		mainBox.setBounds(202, 10, 200, 200);
+        mainBox.setAlignmentX(Component.CENTER_ALIGNMENT);
+        mainBox.setBackground(DARK_BG_COLOR);
+
+        TitledBorder titleBorder = BorderFactory.createTitledBorder("Change Password");
+        titleBorder.setTitleColor(DARK_FG_COLOR);
+        mainBox.setBorder(titleBorder);
 
 		JCustomPasswordField oldPasswordField = this.AddPasswordFieldToBox(OLD_PASSWORD_LABEL, mainBox);
 		JCustomPasswordField newPasswordField = this.AddPasswordFieldToBox(NEW_PASSWORD_LABEL, mainBox);
 		JCustomPasswordField repeatPasswordField = this.AddPasswordFieldToBox(REPEAT_NEW_PASSWORD_LABEL, mainBox);
 
 		JButton confirm = new JButton("Confirm");
+		confirm.setAlignmentX(Component.CENTER_ALIGNMENT);
 		confirm.addActionListener(e -> {
 
 			String oldPassword = new String(oldPasswordField.getPassword());
@@ -70,30 +83,36 @@ public class PasswordUpdatePanel extends JPanel {
 			}
 		});
 
-		mainBox.add(Box.createHorizontalStrut(60));
-		mainBox.add(confirm);
+		errorMessageLabel = new JLabel("");
+        errorMessageLabel.setForeground(Color.RED);
+        errorMessageLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-		add(mainBox);
+        this.add(Box.createVerticalStrut(20));
+        this.add(mainBox);
+        this.add(Box.createVerticalStrut(10));
+        this.add(errorMessageLabel);
+        this.add(Box.createVerticalStrut(10));
+        this.add(confirm);
+        this.add(Box.createVerticalGlue());
 	}
 
 	private JCustomPasswordField AddPasswordFieldToBox(String label, Box mainBox) {
 		JCustomPasswordField field = new JCustomPasswordField(20);
-		field.setPlaceholder(label);
-		mainBox.add(field);
-		mainBox.add(Box.createVerticalStrut(10));
-		return field;
+        field.setPlaceholder(label);
+        field.setMaximumSize(field.getPreferredSize());
+        field.setAlignmentX(Component.CENTER_ALIGNMENT);
+        mainBox.add(field);
+        mainBox.add(Box.createVerticalStrut(10));
+        return field;
 	}
 
 	private void setError(String message){
-		//TODO: Implement error message
+		errorMessageLabel.setText(message);
 	}
 
 	@Override
 	public void paintComponent(Graphics g) {
 		g.setColor(new Color(237, 237, 237));
 		g.fillRoundRect(0, 0, this.getWidth(), this.getHeight(), 30, 25);
-
-		Image image = AssetsRetriever.retrieveAssetImageFromResources("assets/background/page.png");
-		g.drawImage(image, 50, 250, this);
 	}
 }
