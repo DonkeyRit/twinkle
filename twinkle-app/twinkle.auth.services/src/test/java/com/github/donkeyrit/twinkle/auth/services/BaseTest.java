21a4ac9056f748fda9ebbf8c9100a82de01932c5
Dima Alekseev <alekseevda24@gmail.com>
2024-04-01 18:29:27 +0400

    [Twinkle#67] Reference dal.abstractions project from auth project

--- twinkle-app/twinkle.auth.services/src/test/java/com/github/donkeyrit/twinkle/auth/services/BaseTest.java
+++ twinkle-app/twinkle.auth.services/src/test/java/com/github/donkeyrit/twinkle/auth/services/BaseTest.java
@@ -1,27 +1,23 @@
 package com.github.donkeyrit.twinkle.auth.services;
 
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
-
+//TODO: Update tests
 public class BaseTest {
 
-    protected static SessionFactory sessionFactory;
+    // protected static SessionFactory sessionFactory;
 
-    @BeforeSuite
-    public static void setUpBeforeSuite() {
-        try {
-            sessionFactory = new Configuration().configure().buildSessionFactory();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    // @BeforeSuite
+    // public static void setUpBeforeSuite() {
+    //     try {
+    //         sessionFactory = new Configuration().configure().buildSessionFactory();
+    //     } catch (Exception e) {
+    //         e.printStackTrace();
+    //     }
+    // }
 
-    @AfterSuite
-    public static void tearDownAfterSuite() {
-        if (sessionFactory != null) {
-            sessionFactory.close();
-        }
-    }
+    // @AfterSuite
+    // public static void tearDownAfterSuite() {
+    //     if (sessionFactory != null) {
+    //         sessionFactory.close();
+    //     }
+    // }
 }
