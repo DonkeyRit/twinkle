d10197a0c4afed55ad319d45eda78e114240fb7b
Dima Alekseev <alekseevda24@gmail.com>
2025-10-04 12:53:08 +0400

    [Ticket-0000][DEV] Migrate MyTableModel off DataBase onto Hibernate
    MyTableModel ran arbitrary "SELECT * FROM <table>" plus JDBC
    DatabaseMetaData introspection against a raw DriverManager connection
    opened by DataBase. Since the table name is caller-supplied and not a
    fixed entity, there's no single Hibernate entity to query against, so
    the fix is to run the same JDBC work through the connection Hibernate
    already manages: EntityManager is unwrapped to a Session and the
    query/column-introspection run inside Session#doWork, scoped to each
    call instead of holding one connection/ResultSet open for the table
    model's lifetime.

--- twinkle-app/twinkle.main/src/main/java/com/github/donkeyrit/twinkle/controls/MyTableModel.java
+++ twinkle-app/twinkle.main/src/main/java/com/github/donkeyrit/twinkle/controls/MyTableModel.java
@@ -1,75 +1,84 @@
 package com.github.donkeyrit.twinkle.controls;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import javax.swing.table.AbstractTableModel;
 
-import com.github.donkeyrit.twinkle.DataBase;
+import jakarta.persistence.EntityManager;
+
+import org.hibernate.Session;
 
 public class MyTableModel extends AbstractTableModel {
 
-	private String tableName;
-	private String query;
-	private ResultSet rs;
-	private final DataBase database;
+	private final EntityManager entityManager;
+	private final String tableName;
+	private final String query;
 
-	public MyTableModel(DataBase database, String name) 
+	public MyTableModel(EntityManager entityManager, String name)
 	{
-		this.database = database;
-		tableName = name;
-		query = "SELECT * FROM " + tableName;
-		rs = database.select(query);
+		this.entityManager = entityManager;
+		this.tableName = name;
+		this.query = "SELECT * FROM " + tableName;
 	}
 
 	@Override
 	public int getRowCount() {
-		int size = 0;
-		try {
-			rs.last();
-			size = rs.getRow();
-			rs.beforeFirst();
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-		}
-		return size;
+		return getRows().size();
 	}
 
 	@Override
 	public int getColumnCount() {
-		int sizeRows = 0;
-		try {
-			sizeRows = rs.getMetaData().getColumnCount();
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-		}
-		return sizeRows;
+		return getColumnNames().size();
 	}
 
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
-		ArrayList<ArrayList<Object>> allRows = new ArrayList<ArrayList<Object>>();
-		try {
-			while (rs.next()) {
-				ArrayList<Object> oneRow = new ArrayList<Object>();
-				int sizeRows = rs.getMetaData().getColumnCount();
-				for (int i = 1; i < sizeRows + 1; i++) {
-					oneRow.add(rs.getObject(i));
-				}
-				allRows.add(oneRow);
-			}
-			rs.beforeFirst();
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-		}
-		return allRows.get(rowIndex).get(columnIndex);
+		return getRows().get(rowIndex).get(columnIndex);
 	}
 
 	@Override
 	public String getColumnName(int c) {
-		ArrayList<String> names = database.getNameRows(tableName);
-		return names.get(c);
+		return getColumnNames().get(c);
+	}
+
+	private ArrayList<ArrayList<Object>> getRows() {
+		ArrayList<ArrayList<Object>> allRows = new ArrayList<ArrayList<Object>>();
+		entityManager.unwrap(Session.class).doWork(connection -> {
+			try (Statement statement = connection.createStatement();
+					ResultSet rs = statement.executeQuery(query)) {
+				int columnCount = rs.getMetaData().getColumnCount();
+				while (rs.next()) {
+					ArrayList<Object> oneRow = new ArrayList<Object>();
+					for (int i = 1; i <= columnCount; i++) {
+						oneRow.add(rs.getObject(i));
+					}
+					allRows.add(oneRow);
+				}
+			} catch (SQLException ex) {
+				ex.printStackTrace();
+			}
+		});
+		return allRows;
+	}
+
+	private ArrayList<String> getColumnNames() {
+		ArrayList<String> names = new ArrayList<String>();
+		entityManager.unwrap(Session.class).doWork((Connection connection) -> {
+			DatabaseMetaData dbmd = connection.getMetaData();
+			try (ResultSet rs = dbmd.getColumns(null, null, tableName, null)) {
+				while (rs.next()) {
+					names.add(rs.getString("COLUMN_NAME"));
+				}
+			} catch (SQLException ex) {
+				ex.printStackTrace();
+			}
+		});
+		return names;
 	}
 
 }
